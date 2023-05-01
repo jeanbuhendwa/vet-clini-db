fa64f845689fc5c6ab082461f86fb63b5efd8f59
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-01 18:37:55 +0300

    Add schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    Id SERIAL PRIMARY KEY, 
+    Name varchar(100) NOT NULL, 
+    Date_of_Birth DATE NOT NULL, 
+    Escape_Attempts INT NOT NULL, 
+    Neutered BOOLEAN, 
+    Weight_kg DECIMAL(10, 2) NOT NULL
 );
