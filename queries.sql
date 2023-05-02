90fdd250d4c3803bbfef6ab2a4990604166f15c7
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-02 15:02:20 +0300

    delete all data in transaction and rollback

--- queries.sql
+++ queries.sql
@@ -33,6 +33,20 @@ ROLLBACK;
 
 -- nside a transaction:
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
 BEGIN;
 
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+COMMIT; 
+
+
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
