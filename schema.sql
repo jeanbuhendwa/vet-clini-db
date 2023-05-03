a1d023eea1dca57660c054b4816611b43539ff23
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-03 14:10:45 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,17 +9,14 @@ CREATE TABLE animals (
     Weight_kg DECIMAL(10, 2) NOT NULL
 );
 
--- Adding a new column to the animals table
-ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+-- Remove column species
+ALTER TABLE animals DROP COLUMN species;
 
 -- Create a table named owners
 CREATE TABLE owners (Id SERIAL PRIMARY KEY, full_name VARCHAR(255) NOT NULL, age INT);
 
 -- Create a table named species
 CREATE TABLE species(Id SERIAL PRIMARY KEY, name VARCHAR(255));
-
--- Remove column species
-ALTER TABLE animals DROP COLUMN species;
 
 -- Add column species_id which is a foreign key referencing species table
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(Id);
